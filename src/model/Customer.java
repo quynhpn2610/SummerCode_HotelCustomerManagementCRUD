9f5b035bc2cf2bd466d30625ecebc36fc1a4f38f
quynhpn2610 <phuongquynhnguyen.2610@gmail.com>
2021-06-08 16:24:38 +0700

    Customer Entities implemented

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,49 @@
 package model;
 
 public class Customer {
+    private String name;
+    private String id;
+    private int lengthOfStay;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getLengthOfStay() {
+        return lengthOfStay;
+    }
+
+    public void setLengthOfStay(int lengthOfStay) {
+        this.lengthOfStay = lengthOfStay;
+    }
+
+    public Customer(String name, String id, int lengthOfStay) {
+        this.name = name;
+        this.id = id;
+        this.lengthOfStay = lengthOfStay;
+    }
+
+    public Customer() {
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                ", lengthOfStay=" + lengthOfStay +
+                '}';
+    }
 }
