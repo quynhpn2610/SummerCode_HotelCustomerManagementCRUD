c697318585a3fe34464cff1bdf2fb0bedf6d1000
quynhpn2610 <phuongquynhnguyen.2610@gmail.com>
2021-06-08 16:41:23 +0700

    add Customer Service Interface

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -3,4 +3,14 @@ package service;
 import model.Room;
 
 public class CustomerService {
+    static Room[] rooms = new Room[3];
+    static {
+        rooms[0] = new Room(1, 100000);
+        rooms[1] = new Room(2, 200000);
+        rooms[3] = new Room(3, 500000);
+    }
+
+//    dien thong tin
+//    select room -> 2
+//    rooms[1].addCustomer(c);
 }
--- src/service/ICustomerService.java
+++ src/service/ICustomerService.java
@@ -1,4 +1,15 @@
 package service;
 
+import model.Customer;
+
 public interface ICustomerService {
+    void saveCustomer(Customer c);
+
+    void showAllCustomer();
+
+    void updateCustomer(int id, String newName, String newId, int newLengthofStay);
+
+    void deleteCustomer(int id);
+
+    void showCustomer(int id);
 }
