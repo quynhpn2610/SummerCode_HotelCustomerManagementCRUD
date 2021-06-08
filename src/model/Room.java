7abeff786efbc33e8b100716e5e49a22a18fb4e3
quynhpn2610 <phuongquynhnguyen.2610@gmail.com>
2021-06-08 16:28:14 +0700

    Room entities implemented

--- src/model/Room.java
+++ src/model/Room.java
@@ -1,4 +1,38 @@
 package model;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Room {
+    private int roomType;
+    private double roomRate;
+    private ArrayList<Customer> customers = new ArrayList<>();
+
+    public int getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(int roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getRoomRate() {
+        return roomRate;
+    }
+
+    public void setRoomRate(double roomRate) {
+        this.roomRate = roomRate;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return (ArrayList<Customer>) customers.clone();
+    }
+
+    public void setCustomers(ArrayList<Customer> customers) {
+        this.customers = customers;
+    }
+
+    public void addCustomers(Customer c) {
+        customers.add(c);
+    }
 }
