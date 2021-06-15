f8925117e02fc4e5876a184a53a7b65b412ceaba
quynhpn2610 <phuongquynhnguyen.2610@gmail.com>
2021-06-15 18:44:16 +0700

    add Serializable. Check in and ShowAll ok.

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,6 +1,8 @@
 package model;
 
-public class Customer {
+import java.io.Serializable;
+
+public class Customer implements Serializable {
     private String name;
     private int id;
     private int lengthOfStay;
